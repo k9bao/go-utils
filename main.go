b85ebe8900f7e36e2477570a11b1125117b1daf2
keke <keke@mail.com>
2022-08-01 08:27:49 +0800

    test lib

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 import "go-utils/src/fs"
+import "go-utils/src/hash"
+import "go-utils/src/httputil"
 
 func main(){
 	fmt.Println("hello world")
 	fmt.Println(fs.GetParentAbsDir("."))
+	fmt.Println(hash.MD5("12345"))
+	fmt.Println(httputil.GetDefaultHeader())
 }
